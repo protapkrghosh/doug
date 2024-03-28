c3a7c6c907c16c647c67ac457b61aa333828daf1
Protap Ghosh <protapg518@gmail.com>
2024-03-29 02:13:00 +0600

    added how to buy section

---
+++ src/components/sections/HowToBuy/HowToBuy.jsx
@@ -0,0 +1,21 @@
+import dougImg from '@/assets/howToBuy/Doug_DALL.png';
+import shapImg from '@/assets/howToBuy/Group.png';
+import lineImg from '@/assets/howToBuy/Line 15.png';
+import lineLgImg from '@/assets/howToBuy/Linelg.png';
+import Container from '../Container/Container';
+
+const HowToBuy = () => {
+  return (
+    <div>
+      <Container>
+        <h1 className='text-[#111] text-[28px] md:text-[60px] xl:text-[75px] text-center font-montserrat font-bold textShadowSm md:textShadow mb-16'>HOW TO BUY</h1>
+
+        <div className='lg:flex justify-between items-center'>
+
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default HowToBuy;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/sections/Banner/Banner';
 import BirthLegend from '@/components/sections/BirthLegend/BirthLegend';
 import BuyAndSell from '@/components/sections/BuyAndSell/BuyAndSell';
+import HowToBuy from '@/components/sections/HowToBuy/HowToBuy';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <BirthLegend />
       <BuyAndSell />
+      <HowToBuy />
     </>
   );
 };
