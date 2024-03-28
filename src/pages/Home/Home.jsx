013fc04fb9fca02370de1b064b849c58e24dbb8a
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-29 01:55:57 +0600

    aa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/sections/Banner/Banner';
 import BuyAndSell from '@/components/sections/BuyAndSell/BuyAndSell';
+import JoinTheRevolution from '@/components/sections/JoinTheRevolution/JoinTheRevolution';
 
 const Home = () => {
   return (
     <>
       <Banner />
       <BuyAndSell />
+      <JoinTheRevolution />
     </>
   );
 };
