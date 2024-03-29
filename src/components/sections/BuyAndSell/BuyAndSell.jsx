ea0798f7554d1b1ebd76841d71791934dcc38dc9
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-29 22:31:17 +0600

    aa

--- src/components/sections/BuyAndSell/BuyAndSell.jsx
+++ src/components/sections/BuyAndSell/BuyAndSell.jsx
@@ -8,19 +8,19 @@ const BuyAndSell = () => {
   return (
     <div className='my-[75px]'>
       <Container>
-        <div className='flex justify-center items-center relative'>
+        <div className='lg:flex justify-center items-center relative'>
 
           {/* text part  */}
-          <div className='xl:w-3/5'>
+          <div className='xl:w-3/5 lg:w-1/2'>
 
             <div>
-              <p className='2xl:text-[80px] xl:text-[67px] text-[#111] textShadow font-montserrat font-bold leading-[96px] tracking-[1.6px]'>1% BUY & SELL FEE</p>
+              <p className='2xl:text-[80px] xl:text-[67px] lg:text-[45px] md:text-[60px] text-[28px] text-[#111] textShadow font-montserrat font-bold leading-[96px] xl:tracking-[1.6px]'>1% BUY & SELL FEE</p>
 
-              <p className='lg:text-[20px] mt-[25px] mb-[50px] text-[#333131] font-karla font-medium leading-[30px] 2xl:max-w-[688px] xl:w-[665px]'>Doug’s itsy-bitsy fee can potentially elevate the investment experience and outcomes. With redistribution to liquidity, marketing, and burning, the aim is to bolster a more robust and sustainable ecosystem for $DOUG meme coin buyers.</p>
+              <p className='lg:text-[20px] md:mt-[25px] mt-[10px] md:mb-[50px] mb-[24px] text-[#333131] font-karla font-medium leading-[30px] 2xl:max-w-[688px] xl:w-[665px]'>Doug’s itsy-bitsy fee can potentially elevate the investment experience and outcomes. With redistribution to liquidity, marketing, and burning, the aim is to bolster a more robust and sustainable ecosystem for $DOUG meme coin buyers.</p>
 
               <Button className="uppercase px-6">Get some $doug</Button>
 
-              <div className='2xl:w-auto xl:w-[280px] xl:absolute 2xl:-bottom-12 xl:-bottom-12 2xl:left-[265px] xl:left-[265px]'>
+              <div className='hidden lg:block 2xl:w-auto xl:w-[280px] lg:w-[180px] lg:absolute 2xl:-bottom-12 xl:-bottom-12 lg:bottom-10 2xl:left-[265px] xl:left-[265px] lg:left-[205px]'>
                 <img src={buyAndSellDougIcon} alt="" />
               </div>
             </div>
@@ -29,8 +29,8 @@ const BuyAndSell = () => {
 
           {/* image part  */}
 
-          <div className='xl:w-2/5 '>
-            <div className='2xl:w-[750px] xl:w-[500px] xl:absolute 2xl:-bottom-6 xl:bottom-5 2xl:-right-6 xl:-right-12'>
+          <div className='xl:w-2/5 lg:w-1/2'>
+            <div className='2xl:w-[750px] xl:w-[500px] lg:w-[490px] md:w-auto w-auto lg:absolute 2xl:-bottom-6 xl:bottom-5 lg:bottom-4 2xl:-right-6 xl:-right-12 lg:-right-10'>
               <img src={buyAndSellDoug} alt="image" />
             </div>
           </div>
