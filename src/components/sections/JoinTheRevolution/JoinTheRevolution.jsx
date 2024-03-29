db7ea71969e07cad042e20a75afe7ebd0923d338
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-30 02:11:24 +0600

    aa

--- src/components/sections/JoinTheRevolution/JoinTheRevolution.jsx
+++ src/components/sections/JoinTheRevolution/JoinTheRevolution.jsx
@@ -8,11 +8,11 @@ const JoinTheRevolution = () => {
   return (
     <div className='my-[75px]'>
       <Container>
-        <div className='flex items-center xl:gap-[105px]'>
+        <div className='flex items-center xl:gap-[105px] lg:gap-[80px]'>
           {/* images section  */}
           <div className='lg:w-1/2'>
             <div>
-              <img src={JoinTheRevolutionImg} alt="" />
+              <img className='xl:w-auto lg:w-[95%]' src={JoinTheRevolutionImg} alt="" />
               <p className='xl:mt-[-15px] lg:mb-[30px] text-[#333131] text-[20px] font-semibold leading-[30px]'>Join today. Revolutionize your crypto journey. Buy now</p>
               <Button className="uppercase px-6">Get some $doug</Button>
             </div>
@@ -21,23 +21,23 @@ const JoinTheRevolution = () => {
           {/* contant section  */}
           <div className='lg:w-1/2'>
             <div className='relative'>
-              <p className='text-[#111] textShadow font-montserrat xl:text-[80px] font-bold leading-[96px] tracking-[1.6px] mb-[36px]'>JOIN THE REVOLUTION</p>
+              <p className='text-[#111] textShadow font-montserrat xl:text-[80px] lg:text-[50px] font-bold xl:leading-[96px] lg:leading-[75px] tracking-[1.6px] mb-[36px]'>JOIN THE REVOLUTION</p>
 
               {/* large device line  */}
               {/* 1 */}
-              <div className='absolute 2xl:bottom-[266px] xl:bottom-[266px] 2xl:left-[20px] xl:left-[20px]'>
+              <div className='absolute 2xl:bottom-[266px] xl:bottom-[266px] lg:bottom-[277px] 2xl:left-[20px] xl:left-[20px] lg:left-[20px]'>
                 <img src={largeDevice} alt="" />
               </div>
               {/* 2 */}
-              <div className='absolute 2xl:bottom-[190px]  xl:bottom-[190px] 2xl:left-[20px] xl:left-[20px]'>
+              <div className='absolute 2xl:bottom-[190px]  xl:bottom-[190px] lg:bottom-[195px] 2xl:left-[20px] xl:left-[20px] lg:left-[20px]'>
                 <img src={largeDevice} alt="" />
               </div>
               {/* 3 */}
-              <div className='absolute 2xl:bottom-[118px] xl:bottom-[118px] 2xl:left-[20px] xl:left-[20px]'>
+              <div className='absolute 2xl:bottom-[118px] xl:bottom-[118px] lg:bottom-[116px] 2xl:left-[20px] xl:left-[20px] lg:left-[20px]'>
                 <img src={largeDevice} alt="" />
               </div>
               {/* 4 */}
-              <div className='absolute 2xl:bottom-[42px] xl:bottom-[42px] 2xl:left-[20px] xl:left-[20px]'>
+              <div className='absolute 2xl:bottom-[42px] xl:bottom-[42px] lg:bottom-[42px] 2xl:left-[20px] xl:left-[20px] lg:left-[20px]'>
                 <img src={largeDevice} alt="" />
               </div>
 
