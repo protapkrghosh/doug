611337348d828b4644735dbe4d373fdb902e80e1
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-30 13:27:28 +0600

    moon mission section

---
+++ src/components/sections/MoonMisssion/MoonMisssion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoonMisssion = () => {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default MoonMisssion;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,14 @@ import DougMap from '@/components/sections/DougMap/DougMap';
 import Faq from '@/components/sections/Faq/Faq';
 import HowToBuy from '@/components/sections/HowToBuy/HowToBuy';
 import JoinTheRevolution from '@/components/sections/JoinTheRevolution/JoinTheRevolution';
+import MoonMisssion from '@/components/sections/MoonMisssion/MoonMisssion';
 
 const Home = () => {
   return (
     <>
       <Banner />
       <BirthLegend />
+      <MoonMisssion />
       <BuyAndSell />
       <DougMap />
       <HowToBuy />
