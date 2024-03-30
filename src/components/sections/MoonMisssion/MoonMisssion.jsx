5d41ec4617ca8812ca15d1fabaf4fc171e081b1d
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-31 01:23:16 +0600

    aa

--- src/components/sections/MoonMisssion/MoonMisssion.jsx
+++ src/components/sections/MoonMisssion/MoonMisssion.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import Container from '../Container/Container';
 import moonimageIcon from '@/assets/moonMisson/Group.png';
-import moonimage from '@/assets/moonMisson/Groups237835.png';
+import moonimageLarge from '@/assets/moonMisson/Groups237835.png';
+import moonimageSmall from '@/assets/moonMisson/Group237835.png';
 
 const MoonMisssion = () => {
   return (
-    <div className='xl:pt-[320px] lg:pt-[120px] xl:mb-[190px] lg:mb-[190px]'>
+    <div className='xl:pt-[320px] lg:pt-[120px] md:pt-[70px] xl:mb-[190px] lg:mb-[190px] md:mb-[170px]'>
       <Container>
 
         {/* section title  */}
         <div>
-          <p className='text-[#111] textShadow xl:text-[80px] lg:text-[60px] font-bold leading-[96px] tracking-[1.6px] mb-[25px]'>MOON MISSION</p>
+          <p className='text-[#111] textShadow xl:text-[80px] md:text-[60px] font-bold leading-[96px] tracking-[1.6px] lg:mb-[25px] md:mb-[10px]'>MOON MISSION</p>
           <p className='text-[#333131] text-[24px] font-karla font-medium leading-[36px]'>To Infinity and Beyond!</p>
-          <img className='2xl:w-[340px] xl:w-[320px] lg:w-[290px] 2xl:ml-1 xl:ml-4 lg:ml-[26px]' src={moonimageIcon} alt="image" />
+          <img className='hidden lg:block 2xl:w-[340px] xl:w-[320px] lg:w-[290px] 2xl:ml-1 xl:ml-4 lg:ml-[26px]' src={moonimageIcon} alt="image" />
         </div>
 
-        {/* section images  */}
-        <div className='2xl:ml-[-20px] xl:ml-[-20px] 2xl:mt-[-700px] xl:mt-[-550px] lg:mt-[-350px]'>
-          <img src={moonimage} alt="image" />
+        {/* section images large */}
+        <div className='hidden lg:block 2xl:ml-[-20px] xl:ml-[-20px] 2xl:mt-[-700px] xl:mt-[-550px] lg:mt-[-350px]'>
+          <img src={moonimageLarge} alt="image" />
+        </div>
+
+        {/* section images small */}
+        <div className='block lg:hidden md:w-auto'>
+          <img className='mx-auto md:w-[400px]' src={moonimageSmall} alt="image" />
         </div>
 
         {/* section text content */}
