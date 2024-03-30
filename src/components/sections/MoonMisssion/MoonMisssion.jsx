90f981e9816d7759b1cf303c0b19d9464bfbcfa7
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-30 13:41:56 +0600

    aa

--- src/components/sections/MoonMisssion/MoonMisssion.jsx
+++ src/components/sections/MoonMisssion/MoonMisssion.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Container from '../Container/Container';
 
 const MoonMisssion = () => {
   return (
     <div>
-      <p>hello</p>
+      <Container>
+        {/* section title  */}
+
+        <div>
+          <p className='text-[#111] textShadow 2xl:text-[80px] font-bold leading-[96px] tracking-[1.6px] mb-[25px]'>MOON MISSION</p>
+          <p className='text-[#333131] text-[24px] font-karla font-medium leading-[36px]'>To Infinity and Beyond!</p>
+        </div>
+
+        {/* section images  */}
+        <div></div>
+
+        {/* section text content */}
+        <div>
+          <p className='xl:w-[664px] text-[#333131] text-[20px] font-karla font-medium leading-[30px]'>Doug’s here to make crypto fun, accessible, and profitable for everyone, from the seasoned trader to the curious snewcomer. Get Doug now, or risk being left behind.</p>
+        </div>
+      </Container>
     </div>
   );
 };
