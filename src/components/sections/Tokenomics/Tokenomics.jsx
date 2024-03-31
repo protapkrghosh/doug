767049f036c4b53c71748771dfd0fbdf9a046acb
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-31 16:20:22 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Container from '../Container/Container';
 
 const Tokenomics = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <Container>
+        <div>
+          <div>
+            <p className='text-[#111] textShadow font-montserrat text-[80px] font-bold leading-[96px] tracking-[1.6px]'>TOKENOMICS</p>
+            <p className='text-[#FFF] font-karla text-[28px] font-medium leading-[42px]'>Max Supply: 777 trillion.</p>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
