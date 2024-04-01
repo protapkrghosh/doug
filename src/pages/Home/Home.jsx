f11769ce75dce2d3f4da4dbec018bdc5f6b91132
Protap Ghosh <protapg518@gmail.com>
2024-04-01 16:17:36 +0600

    complete moon mission section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import DougMap from '@/components/sections/DougMap/DougMap';
 import Faq from '@/components/sections/Faq/Faq';
 import HowToBuy from '@/components/sections/HowToBuy/HowToBuy';
 import JoinTheRevolution from '@/components/sections/JoinTheRevolution/JoinTheRevolution';
-import MoonMisssion from '@/components/sections/MoonMisssion/MoonMisssion';
+import MoonMisssion from '@/components/sections/MoonMission/MoonMission';
 import Tokenomics from '@/components/sections/Tokenomics/Tokenomics';
 
 const Home = () => {
