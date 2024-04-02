be3f25e497bc6917c6f77c09083f8d6b94d644f9
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-02 21:09:58 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -11,8 +11,8 @@ const Tokenomics = () => {
 
           {/* title section  */}
           <div>
-            <p className='text-[#111] text-center textShadow font-montserrat xl:text-[80px] font-bold lg:leading-[96px] tracking-[1.6px] mb-[25px]'>TOKENOMICS</p>
-            <div className='flex items-center gap-4 w-fit bg-[#EEB10E] mx-auto px-[30px] py-[15px] rounded-full text-[#FFF] font-karla 2xl:text-[28px] xl:text-[24px] font-medium leading-[42px]'>
+            <p className='text-[#111] text-center textShadow font-montserrat xl:text-[80px] lg:text-[60px] font-bold lg:leading-[96px] tracking-[1.6px] mb-[25px]'>TOKENOMICS</p>
+            <div className='flex items-center gap-4 w-fit bg-[#EEB10E] mx-auto px-[30px] py-[15px] rounded-full text-[#FFF] font-karla 2xl:text-[28px] lg:text-[24px] font-medium leading-[42px]'>
               <img src={tokenTitleImg} alt="" />
               <p>Max Supply: 777 trillion.</p>
             </div>
@@ -20,47 +20,47 @@ const Tokenomics = () => {
 
           <div className='flex justify-evenly items-center mt-[135px]'>
             {/* image section  */}
-            <div>
-              <img className='2xl:w-[90%] xl:w-[80%]' src={tokenImg} alt="" />
+            <div className='xl:w-auto lg:w-[60%]'>
+              <img className='2xl:w-[90%] xl:w-[80%] lg:w-[80%]' src={tokenImg} alt="" />
             </div>
 
             {/* image contain section  */}
-            <div>
+            <div className='xl:w-auto lg:w-[40%]'>
 
               {/* 1 button  */}
               <div className='flex items-center gap-[10px] bg-[#EEB10E] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>20% Airdrop</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>20% Airdrop</p>
               </div>
 
               {/* 2 button  */}
               <div className='flex items-center gap-[10px] bg-[#ED730E] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>20% Liquidity</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>20% Liquidity</p>
               </div>
 
               {/* 3 button  */}
               <div className='flex items-center gap-[10px] bg-[#ED970E] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>20% Community Reserve</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>20% Community Reserve</p>
               </div>
 
               {/* 4 button  */}
               <div className='flex items-center gap-[10px] bg-[#31CC20] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>15%  Burn Mechanism</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>15%  Burn Mechanism</p>
               </div>
 
               {/* 5 button  */}
               <div className='flex items-center gap-[10px] bg-[#EDCD0E] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>15% Marketing & Development</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>15% Marketing & Development</p>
               </div>
 
               {/* 6 button  */}
               <div className='flex items-center gap-[10px] bg-[#EDE80E] w-fit py-[15px] px-[20px] rounded-full mb-[20px]'>
                 <div className='h-5 w-5 bg-[#FFF] border-2 border-black rounded-full' />
-                <p className='text-[#111] font-karla text-[28px] leading-[42px]'>10% Early Contributors</p>
+                <p className='text-[#111] font-karla xl:text-[28px] lg:text-[20px] leading-[42px]'>10% Early Contributors</p>
               </div>
 
             </div>
